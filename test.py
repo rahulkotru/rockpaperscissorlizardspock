e8f794c1883df6995859811470516de7b4403376
Rahul <rahulkotr1@gmail.com>
2022-06-30 18:37:04 +0530

    Scaled Image

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ while (True):
     imgBG= cv2.imread("Resources/rock.png")
     imgScaled= cv2.resize(imgBG,(0,0),None,0.875,0.875)
     imgScaled=imgScaled[:,80:480]
+    imgBG[]=
     success, img=cap.read()
     cv2.imshow("image",img)
     cv2.imshow("BG",imgBG)
